ffac02520664043fa46eec3eb0aaf22c9f899d7e
Julien Levesy <jlevesy@gmail.com>
2015-06-02 09:56:50 +0200

    more gulpfile cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -14,38 +14,38 @@ var resources = [
   "icons/icon16.png",
   "icons/icon48.png",
   "icons/icon128.png",
-  "./bower_components/jquery/dist/jquery.min.js",
-  "./bower_components/gmail.js/src/gmail.js",
-  "./content.js",
-  "./manifest.json"
+  "bower_components/jquery/dist/jquery.min.js",
+  "bower_components/gmail.js/src/gmail.js",
+  "content.js",
+  "manifest.json"
 ]
 
 gulp.task('template', function() {
   gulp.src(
-    './src/*.coffee'
+    'src/*.coffee'
   ).pipe(coffee({bare: true})).on('error', gutil.log)
    .pipe(replace(
     'API_ENDPOINT', process.env.API_ENDPOINT || 'http://localhost:3000'
   )).pipe(replace(
     'APP_ENDPOINT', process.env.APP_ENDPOINT || 'http://localhost:4200'
   )).pipe(concat('main.js'))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('bower', function() {
   bower();
 });
 
-gulp.task('watch', function() {
-  gulp.watch('./src/*.coffee', ['template']);
-  gulp.watch('./bower.json', ['bower']);
+gulp.task('watch', ['package'], function() {
+  gulp.watch('src/*.coffee', ['template']);
+  gulp.watch('bower.json', ['bower']);
 });
 
-gulp.task('default', ['bower', 'template', 'watch'], function() {});
+gulp.task('default', ['bower', 'watch'], function() {});
 
 gulp.task('clean', function() {
-  gulp.src('./dist/*', {read: false}).pipe(clean())
-  gulp.src('./*.crx', {read: false}).pipe(clean())
+  gulp.src('dist/*', {read: false}).pipe(clean())
+  gulp.src('*.crx', {read: false}).pipe(clean())
 })
 
 gulp.task('package', ['template'], function() {
