e8b0bebbe1930f00765ff3cb504b8295f39eaef5
Julien Levesy <jlevesy@gmail.com>
2015-09-03 18:54:45 -0700

    bump version

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ var gulp = require('gulp'),
   buffer = require('vinyl-buffer'),
   es = require('event-stream');
 
-var version = '0.0.28';
+var version = '0.0.29';
 
 var pathmodify_mapping = [
   pathmodify.mod.dir('app',path.join(__dirname, 'src')),
